b2d0d568a462aecb5c06329ad5461800f35b0005
Kishor V <developer.kishor@gmail.com>
2021-01-10 23:14:56 +0530

    make it simple

--- install-sd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-install -Dm644 ./install/sd-clevis /etc/initcpio/install/
-install -Dm644 ./systemd-clevis-unlock.path /etc/systemd/system/systemd-clevis-unlock.path
-install -Dm644 ./systemd-clevis-unlock.service /etc/systemd/system/systemd-clevis-unlock.service
